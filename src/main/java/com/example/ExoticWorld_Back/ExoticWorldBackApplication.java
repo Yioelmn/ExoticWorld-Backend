abb204f80fe22ca59b482d78f13f1753f274703e
Yioelmn <jo.calfuqueo@duocuc.cl>
2025-11-18 17:03:38 -0300

    Primer commit

---
+++ src/main/java/com/example/ExoticWorld_Back/ExoticWorldBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.ExoticWorld_Back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExoticWorldBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExoticWorldBackApplication.class, args);
+	}
+
+}
